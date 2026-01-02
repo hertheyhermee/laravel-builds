477f1806b8ab4cbb6aa0264a4a2b54c0ee9942a4
hertheyhermee <tiamiyuhayzed@gmail.com>
2026-01-02 23:43:45 +0000

    controller logic for authentication inplemented

--- auth-system/app/Http/Controllers/AuthController.php
+++ auth-system/app/Http/Controllers/AuthController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Auth\Services\RegisterUserService;
+use App\Auth\Tokens\TokenIssuer;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(
+        Request $request, 
+        RegisterUserService $registerUserService,
+        TokenIssuer $tokenIssuer
+    ){
+        $data =$request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+        $user = $registerUserService->register(
+            $data['name'],
+            $data['email'],
+            $data['password'],  
+        );
+        $token = $tokenIssuer->issueToken($user);
+        return response()->json([
+            'token' => $token,
+        ], 201);
+        
+    }
 }
